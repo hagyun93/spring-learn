2b55999879b3264e295229961ddbc4b74f1d11e5
hagyun93 <imkimhagyun@naver.com>
2023-03-05 18:26:17 +0900

    Proxy, Porxy Pattern Base Project create

---
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -0,0 +1,17 @@
+package hello.proxy;
+
+import hello.proxy.app.config.AppV1Config;
+import hello.proxy.app.config.AppV2Config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
+public class ProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
+}
